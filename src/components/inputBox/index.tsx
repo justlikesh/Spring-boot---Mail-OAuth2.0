0714662ab6b8c10ad4030e90bb69d3e9eb8d7e22
justlikesh <lpsac@naver.com>
2024-09-05 21:26:09 +0900

    Feat: InputBox 컴포넌트 구현
    - forwardRef를 이용한 InputBox 컴포넌트 생성
    - Props로 입력 값, 메시지, 버튼 클릭 등의 이벤트 처리 추가
    - 조건부 렌더링을 통해 버튼과 메시지 표시 제어
    - 입력 값에 따른 버튼 활성화/비활성화 상태 처리
    - 에러 메시지 표시를 위한 스타일링 추가

--- src/components/inputBox/index.tsx
+++ src/components/inputBox/index.tsx
@@ -1,23 +1,64 @@
-import { forwardRef } from "react";
+import {
+  ChangeEvent,
+  forwardRef,
+  KeyboardEvent as ReactKeyboardEvent,
+} from "react";
 import "./style.css";
 
-interface Props {}
+interface Props {
+  title: string;
+  placeholder: string;
+  type: "text" | "password";
+  value: string;
+  message?: string;
+  isErrorMessage?: boolean;
+  buttonTitle?: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  onKeyDown?: (event: ReactKeyboardEvent<HTMLInputElement>) => void;
+  onButtonClick?: () => void;
+}
 
 const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
+  const {
+    title,
+    placeholder,
+    type,
+    value,
+    isErrorMessage,
+    buttonTitle,
+    message,
+    onChange,
+    onKeyDown,
+    onButtonClick,
+  } = props;
+
+  const buttonClass =
+    value === "" ? "input-box-button-disable" : "input-box-button";
+  const messageClass = isErrorMessage
+    ? "input-box-message-error"
+    : "input-box-message";
+
   return (
     <div className="input-box">
-      <div className="input-box-title">{"아이디"}</div>
+      <div className="input-box-title">{"title"}</div>
       <div className="input-box-content">
         <div className="input-box-body">
           <input
+            ref={ref}
             className="input-box-input"
-            placeholder="아이디를 입력해주세요"
+            placeholder={placeholder}
+            type={type}
+            value={value}
+            onChange={onChange}
+            onKeyDown={onKeyDown}
           />
-          <div className="input-box-button-disable">{"중복 확인"}</div>
+          {buttonTitle !== undefined && onButtonClick !== undefined && (
+            <div className={buttonClass} onClick={onButtonClick}>
+              {buttonTitle}
+            </div>
+          )}
         </div>
-        <div className="input-box-message-error">
-          {"사용 가능한 아이디 입니다."}
-        </div>
+        {message !== undefined && <div className={messageClass}>{message}</div>}
       </div>
     </div>
   );
