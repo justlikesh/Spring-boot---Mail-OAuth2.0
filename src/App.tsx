1bab06aac96eacd9358b4467cece3addd718bd65
justlikesh <lpsac@naver.com>
2024-09-05 22:07:12 +0900

    Feat : 공백 추가
    - 테스트를 위한 공백 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import InputBox from "components/inputBox";
 
 function App() {
+  const [id, setId] = useState<string>("");
+
+  const onIdChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setId(value);
+  };
+
   return (
     <>
-      <InputBox />
+      <InputBox
+        title="아이디"
+        placeholder="아이디를 입력해주세요"
+        type="text"
+        value={id}
+        onChange={onIdChangeHandler}
+      />
     </>
   );
 }
