74e99a1f054104bdf879ec8f2db59f5bdf366397
justlikesh <lpsac@naver.com>
2024-09-06 19:11:48 +0900

    Feat: Response 관련 타입 및 열거형(enum) 정의
    - ResponseDto 인터페이스 정의 및 export
    - ResponseCode와 ResponseMessage 열거형(enum) 추가
    - 성공, 유효성 검사 실패, 중복 ID, 로그인 실패 등의 코드 및 메시지 열거형(enum) 값 설정

---
+++ src/types/enums/response-message.enum.ts
@@ -0,0 +1,12 @@
+enum ResponseMessage {
+  SUCCESS = "Success.",
+  VALIDATION_FAIL = "Validation Failed.",
+  DUPLICATE_ID = "Duplicate Id.",
+
+  SIGN_IN_FAIL = "Login information mismatch.",
+  CERTIFICATION_FAIL = "Certification Failed.",
+
+  DATABASE_ERROR = "Database error.",
+}
+
+export default ResponseMessage;
