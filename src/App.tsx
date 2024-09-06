2521fd1e2e82948d33c5d873c62dbacf30fbe452
justlikesh <lpsac@naver.com>
2024-09-06 13:36:13 +0900

    Feat: 버튼 및 링크 스타일 추가
    - 기본 큰 사이즈 버튼 .primary-button-lg 스타일 정의
    - 비활성화 버튼 .disable-button-lg 스타일 추가
    - 텍스트 링크 스타일 .text-link-lg 정의 및 hover 효과 추가
    - 카카오 및 네이버 로그인 버튼 스타일 .kakao-sign-in-button, .naver-sign-in-button 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,11 @@ import "./App.css";
 import InputBox from "components/inputBox";
 
 function App() {
-  const [id, setId] = useState<string>("");
-
-  const onIdChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-    setId(value);
-  };
-
-  const onIdButtonClickHandler = () => {};
-
   return (
     <>
-      <InputBox
-        title="아이디"
-        placeholder="아이디를 입력해주세요"
-        type="text"
-        value={id}
-        onChange={onIdChangeHandler}
-        buttonTitle="중복 확인"
-        onButtonClick={onIdButtonClickHandler}
-        message="사용 가능한 아이디 입니다"
-        isErrorMessage={true}
-      />
+      <div className="text-link-lg full-width">회원가입</div>
+      <div className="kakao-sign-in-button"></div>
+      <div className="naver-sign-in-button"></div>
     </>
   );
 }
